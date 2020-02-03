0600f72e4f988c0bcfe3484a09ce993136fb1292
phimm-hub <58954116+phimm-hub@users.noreply.github.com>
2020-02-03 15:07:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "phimm-cicd"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
