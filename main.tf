5b661d0c3d4a3f17e5611e9785f4f43d63c5e807
phimm-hub <58954116+phimm-hub@users.noreply.github.com>
2020-02-03 21:51:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ provider "aws" {
 
 # Create EC2 instance
 resource "aws_instance" "default" {
-  ami                    = var.ami
-  count                  = var.instance_count
+  ami                    = ami-0fc20dd1da406780b
+  count                  = 1
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
   instance_type          = var.instance_type
 
-  tags = {
-    Name = "terraform-default"
-  }
+ # tags = {
+ #   Name = "terraform-default"
+  #}
 }
 
 # Create Security Group for EC2
